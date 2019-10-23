d896f3d355bfe7bd2c31a907af87850f46e798d4
Beer Neko <beerneko@yahoo.com>
2019-10-23 13:55:45 -0500

    The first commit for learning Netlify Large Media

---
+++ src/components/header.js
@@ -0,0 +1,5 @@
+import React from "react"
+export default props => 
+    <div style={{ color: `blue` }}>
+        <h1>{props.headerText}</h1>
+    </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import Header from "../components/header"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div style={{ color: `blue` }}>
+    <Header headerText="Index Page" />
+    <Link to="/contact/">Contact</Link>
+    <p>Such wow. Very React.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <img src="https://source.unsplash.com/user/beerneko/400x200" alt="" />
+    <button class="primary-button">Click me</button>
+    </div>
+)
